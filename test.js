b47b49b2ff3b109836967956b51080e33ab02c6d
Akhilesh Soni <akhileshsoni098@gmail.com>
2023-11-20 11:55:24 +0530

    kkk

---
+++ test.js
@@ -0,0 +1,77 @@
+/* 
+
+Lismy Resume Application:
+
+1. The cover letter is not in the right place.
+
+2. The search bar is not functioning. We need to enable users to select a resume category, and then they can choose a resume based on search filters. The data will be reflected in the resumes, making it easier for users to create their resumes.
+
+3. When I try to fill in data after the Personal Info section, it throws a range error. We need to resolve this issue.
+
+Lismy Resume Website:
+
+1. CVs have not been created yet.
+
+2. The cover letter remains incomplete.
+
+3. There are only two or three resume templates available.
+
+4. The blog page has not been created.
+
+5. Category-wise resume articles are not visible.
+
+6. Mobile responsiveness is pending.
+
+Lismy Resume Admin Panel:
+
+1. The admin panel is not responsive for mobile devices.
+
+2. We need to resolve frontend bugs.
+
+3. Ensure that each and every functionality is working perfectly.
+
+OctaDo Application:
+
+Section A - To-Do Task:
+
+1. Every feature is pending in terms of design and functionality.
+
+Section B - Diary:
+
+1. APIs have not been integrated yet.
+
+2. Special effect features have not been added.
+
+3. Other functionalities seem to work well but need some modifications.
+
+4. The Undo and Redo functions are not working.
+
+5. The template button is not functional.
+
+6. The triple-dot menu on the diary creation page is not working.
+
+Section C - Notes:
+
+Camera:
+
+1. Camera functionality is not complete.
+
+Audio:
+
+1. Audio functionality is not implemented yet.
+
+Handwriting:
+
+1. In the Handwriting functionality, only the pen tool is working. The Background Eraser and All Clear functionalities are not working.
+
+2. Features such as undo, redo, plus, and the triple-dot menu are not added.
+
+Attachment:
+
+1. In this part, writing features for the title and description, as well as image uploading, are completed.
+
+2. Most of the functionality is not working.
+
+Text Note:
+
+1. Only the UI design is completed; no other functionality has been implemented yet. */
